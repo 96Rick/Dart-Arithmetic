11d107a99e325484e97a66f2561d95f7b42b58b6
Rick <632323511@qq.com>
2020-09-08 20:22:18 +0800

    update linkedlist

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,29 @@
+import '../lib/LinkedList.dart';
+
 import '../lib/array.dart';
 
 void main() {
   // 数组插入删除
-  var array = DiscontinuousArray(4);
-  array.insert(0, 1);
-  array.insert(1, 2);
-  array.insert(2, 3);
-  array.insert(3, 4);
-  array.insert(5, 0);
-  array.insert(4, 5);
-  array.insert(2, 10);
-  array.delete(0);
-  array.output();
+  // var array = DiscontinuousArray(4);
+  // array.insert(0, 1);
+  // array.insert(1, 2);
+  // array.insert(2, 3);
+  // array.insert(3, 4);
+  // array.insert(5, 0);
+  // array.insert(4, 5);
+  // array.insert(2, 10);
+  // array.delete(0);
+  // array.output();
+
+  // ignore: omit_local_variable_types
+  MyLinkedList link = MyLinkedList(0);
+  link.insert(0, 0);
+  link.insert(1, 1);
+  link.insert(2, 2);
+  link.insert(3, 3);
+  link.insert(4, 4);
+  link.insert(5, 5);
+  link.insert(6, 6);
+  link.insert(10, 1);
+  link.output();
 }
--- lib/LinkedList.dart
+++ lib/LinkedList.dart
@@ -1,9 +1,81 @@
-class LinkedList {
-  
+class MyLinkedList {
+  Node _head;
+  Node _last;
+  int _size;
+  MyLinkedList(int size) : _size = size;
+
+  void insert(int data, int index) {
+    if (index < 0 || index > _size) {
+      print("插入位置错误");
+      return;
+    }
+    Node newNode = Node(data: data);
+    if (_size == 0) {
+      _head = newNode;
+      _last = newNode;
+    } else if (index == 0) {
+      newNode.nextNode = _head;
+      _head = newNode;
+    } else if (index == _size) {
+      _last.nextNode = newNode;
+      _last = newNode;
+    } else {
+      Node previousNode = get(index);
+      newNode.nextNode = previousNode.nextNode;
+      previousNode.nextNode = newNode;
+    }
+    _size++;
+  }
+
+  Node get(int index) {
+    if (index < 0 || index >= _size) {
+      print("插入位置错误");
+      return null;
+      // throw FormatException("插入位置错误");
+    }
+    Node temp = _head;
+    for (var i = 0; i < index; i++) {
+      temp = temp.nextNode;
+    }
+    return temp;
+  }
+
+  Node remove(index) {
+    if (index < 0 || index >= _size) {
+      print("插入位置错误");
+      return null;
+      // throw FormatException("插入位置错误");
+    }
+    Node deleteNode = null;
+    if (index == 0) {
+      deleteNode = _head;
+      _head = _head.nextNode;
+    } else if (index == _size - 1) {
+      deleteNode = _last;
+      Node previousNode = get(index - 1);
+      previousNode.nextNode = null;
+      _last = previousNode;
+    } else {
+      Node previousNode = get(index - 1);
+      Node nextNode = previousNode.nextNode.nextNode;
+      deleteNode = previousNode.nextNode;
+      previousNode.nextNode = nextNode;
+    }
+    _size--;
+    return deleteNode;
+  }
+
+  void output() {
+    Node temp = _head;
+    while (temp != null) {
+      print(temp.data);
+      temp = temp.nextNode;
+    }
+  }
 }
 
 class Node {
   Node nextNode;
-  dynamic data;
-  Node(this.data,this.nextNode);
+  int data;
+  Node({this.data});
 }
